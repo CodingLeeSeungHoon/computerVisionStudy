ed9647e2b9a3a0e8d7ee40b5f6aa0c1f8ee06415
CodingLeeSeungHoon <free_minkya@naver.com>
2021-03-12 22:52:15 +0900

    add chapter 2

--- 01. OpenCV Basic/OpenCVBasic.py
+++ 01. OpenCV Basic/OpenCVBasic.py
@@ -1,6 +1,7 @@
 import cv2
 import sys
-
+import matplotlib.pyplot as plt
+ESC = 27
 """
 grayscale data : numpy.uint8 (1Byte)
 truecolor data : tuple / numpy.ndarray (3Bytes)
@@ -17,6 +18,31 @@ if img is None:
 
 cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 cv2.imshow('image', img)
-cv2.waitKey()
+while True:
+    if cv2.waitKey() in [ESC, ord('x')]:
+        break
 
 cv2.destroyAllWindows()
+
+
+"""
+matplotlib를 이용한 영상출력
+cv2.imread() : BGR 순서
+matplotlib : RGB 순서 >> cv2.cvtColor() 함수를 사용해서 변경해줌
+
+plt.imshow()에서 cmap='gray' 출력해 grayscale 이미지 출력
+"""
+
+imgBGR = cv2.imread('cat.bmp')
+imgGray = cv2.imread('cat_gray.jpg')
+imgRGB = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
+
+plt.axis('off')
+plt.imshow(imgRGB)
+plt.show()
+
+# 두 개의 영상을 함께 출력
+# 121 : 창 1개에 2개의 영상칸 그 중에 1번째 영상칸 선택
+plt.subplot(121), plt.axis('off'), plt.imshow(imgRGB)
+plt.subplot(122), plt.axis('off'), plt.imshow(imgGray, cmap='gray')
+plt.show()
